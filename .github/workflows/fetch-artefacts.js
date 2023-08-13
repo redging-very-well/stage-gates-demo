af670185a3218a6b0b4c2fcca04013402f37fb73
redging-very-well <Redgwell@gmail.com>
2023-08-14 11:22:44 +1200

    fixes artifact fetch (#3)
    * switch download to use GH object

--- .github/workflows/fetch-artefacts.js
+++ .github/workflows/fetch-artefacts.js
@@ -1,24 +1,17 @@
 const fs = require("fs");
 const path = require("path");
 
-const downloadFile = async (fetch, url, path) => {
-  const res = await fetch(url);
-  const fileStream = fs.createWriteStream(path);
-  await new Promise((resolve, reject) => {
-    res.body.pipe(fileStream);
-    res.body.on("error", reject);
-    fileStream.on("finish", resolve);
-  });
+const downloadFileEx = async (github, url, path) => {
+  console.log(`Fetching artifact from: ${url}`);
+  const res = await github.request(url);
+  if (res.status != 200) {
+    throw new Error(`could not get artifact from ${url}: ${res.data}`);
+  }
+  console.log(`Found artifact with length: ${res.data.byteLength}`);
+  await fs.promises.writeFile(path, Buffer.from(res.data));
 };
 
-module.exports = async ({
-  core,
-  github,
-  context,
-  fetch,
-  runId,
-  destination,
-}) => {
+module.exports = async ({ core, github, context, runId, destination }) => {
   const response = await github.rest.actions.listWorkflowRunArtifacts({
     owner: context.repo.owner,
     repo: context.repo.repo,
@@ -38,7 +31,7 @@ module.exports = async ({
 
     const url = item.archive_download_url;
     const artifactPath = path.join(destination, `${item.name}.zip`);
-    await downloadFile(fetch, url, artifactPath);
+    await downloadFileEx(github, url, artifactPath);
 
     core.info(`Fetched artifact: ${artifactPath}`);
     artefacts.push(artifactPath);
